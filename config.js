250fc75f8b308ad65c86e34a5ae181e05d0ac65e
Mosaab Emam <mosaab.emam123@gmail.com>
2020-01-28 01:57:54 +0200

    configured for heroku deployment

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ const { remoteDBPassword } = require("./private");
 module.exports = {
   PORT: process.env.PORT || 3000,
   mongodb: {
-    url: `mongodb+srv://nuxt-express-todo-app:${remoteDBPassword}@nuxtexpresstodoapp-fz4xb.mongodb.net/todo-app?retryWrites=true&w=majority`,
+    url: `mongodb+srv://nuxt-express-todo-app:${
+      process.env.remoteDBPassword
+    }@nuxtexpresstodoapp-fz4xb.mongodb.net/todo-app?retryWrites=true&w=majority`,
     options: {
       useNewUrlParser: true,
       useFindAndModify: false,
