4095e1115c116ae0374bbbae08c6f51ddac2757f
Mosaab Emam <mosaab.emam123@gmail.com>
2020-01-28 01:37:27 +0200

    Switched to a remote database (Mongo Atlas)

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
+const { remoteDBPassword } = require("./private");
 module.exports = {
   PORT: process.env.PORT || 3000,
   mongodb: {
-    url: "mongodb://localhost:27017/todos",
+    url: `mongodb+srv://nuxt-express-todo-app:${remoteDBPassword}@nuxtexpresstodoapp-fz4xb.mongodb.net/todo-app?retryWrites=true&w=majority`,
     options: {
       useNewUrlParser: true,
       useFindAndModify: false,
--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
-    graphiql: true,
     schema: graphqlSchema
   })
 );
